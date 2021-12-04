9a57f968fba351343d4b0dd2f923ac19acc435fb
edirie <essedirie@gmail.com>
2021-12-04 13:44:50 -0500

    changes

--- script.js
+++ script.js
@@ -3,37 +3,131 @@ var generateBtn = document.querySelector("#generate");
 
 // change all var to string
 var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperChars = ["A", "B","C","D","E","F",G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-var specialChars = "!”#$%&’()*+,-./:;<=>?@][^_`{|}~";
+var upperChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerChars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var specialChars = [
+  "+",
+  "-",
+  "&&",
+  "||",
+  "!",
+  "(",
+  ")",
+  "{",
+  "}",
+  "[",
+  "]",
+  "^",
+  "~",
+  "*",
+  "?",
+  ":",
+];
 
-function formPass() {
+function generatePassword() {
   var length = window.prompt("How many characters between 8 and 128?");
-
   if (length < 8 || length > 128) {
     window.alert("Invalid length selection, please try again.");
+    formPass();
   }
   if (isNaN(length)) {
-    window.alert("Invalid length selection, please try again.");
+    window.alert("Not a number, please try again.");
+    formPass();
   }
 
-    var wantSpecialchar = confirm("If you want special characters, press okay");
-    var wantNumbers = confirm("If you want numbers, press okay");
-    var wantLow = confirm("If you want lowercase characters, press okay");
-    var wantUpper = confirm("If you want uppercase characters, press okay");
+  var wantSpecialchar = confirm("If you want special characters, press okay");
+  var wantNumbers = confirm("If you want numbers, press okay");
+  var wantLow = confirm("If you want lowercase characters, press okay");
+  var wantUpper = confirm("If you want uppercase characters, press okay");
 
-    if (wantSpecialchar === false && wantNumbers === false && wantLow === false && wantUpper === false) {
-        window.alert("Please entere criteria")
-    } 
-    
-    var passProperties = {
-        length: length, 
-        wantSpecialchar: wantSpecialchar,
-        wantLow: wantLow,
-        wantUpper: wantUpper,
-    };
+  if (
+    wantSpecialchar === false &&
+    wantNumbers === false &&
+    wantLow === false &&
+    wantUpper === false
+  ) {
+    window.alert("Please meet the proper criteria");
+  }
 
-    return (passProperties);
+  var conCharacters = [];
+
+  if (wantSpecialchar) {
+     conCharacters = conCharacters.concat(specialChars);
+    //   console.log('wantSpecialChar', conCharacters);
+  }
+
+  if (wantNumbers) {
+      conCharacters = conCharacters.concat(numberChars);
+    //   console.log('wantNumbers', conCharacters);
+  }
+
+  if (wantLow) {
+      conCharacters = conCharacters.concat(lowerChars);
+    //   console.log('wantLow', conCharacters);
+  }
+
+  if (wantUpper) {
+      conCharacters = conCharacters.concat(upperChars);
+    //   console.log('wantUpper', conCharacters);
+  }
+
+  var newPass = conCharacters.sort(() => Math.random() - Math.random() ).slice(0, length)
+
+  return generatePassword;
 }
 
 // Write password to the #password input
@@ -45,4 +139,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", formPass);
+generateBtn.addEventListener("click", writePassword);
